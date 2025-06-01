966e0635d4c2dc3a9f7ef929c3eeac7bd490e907
stephan <ardistephan@gmail.com>
2025-06-01 23:23:01 +0800

    Changes related to project

---
+++ ARIslandGame/ARIslandGameTests/ARIslandGameTests.swift
@@ -0,0 +1,17 @@
+//
+//  ARIslandGameTests.swift
+//  ARIslandGameTests
+//
+//  Created by stephan on 20/05/25.
+//
+
+import Testing
+@testable import ARIslandGame
+
+struct ARIslandGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
